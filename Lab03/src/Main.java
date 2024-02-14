5d7af0ddc2b329b611efe0e8c1216f533d58764a
Jordan Andersen <bokeh.yap.0l@icloud.com>
2024-02-14 11:32:25 -0500

    improved code style: created STRING CONSTANTS for Main class.

--- Lab03/src/Main.java
+++ Lab03/src/Main.java
@@ -2,28 +2,33 @@ import java.io.File;
 import java.util.Scanner;
 
 public class Main {
+    // STRING CONSTANTS
+    static final String USER_QUIT = "quit";
+    static final String USER_PROMPT = "Enter a path to map: ";
+    static final String INVALID_INPUT_ERROR = "\nInvalid input!\n";
+
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
         boolean continueProgram = true;
 
         while (continueProgram) {
             // USER PROMPT AND INPUT
-            System.out.print("Enter a path to map: ");
-            String path = userInput.nextLine().strip();
+            System.out.print(USER_PROMPT);
+            String rootPath = userInput.nextLine().strip();
 
             // INPUT VALIDATION
-            File rootDirectory = new File(path);
+            File rootDirectory = new File(rootPath);
             boolean isDirectory = rootDirectory.isDirectory();
             boolean directoryExists = rootDirectory.exists();
 
             // INPUT LOGIC
-            if (path.equalsIgnoreCase("quit")) {
+            if (rootPath.equalsIgnoreCase(USER_QUIT)) {
                 continueProgram = false;
             } else if (isDirectory && directoryExists) {
-                TreeNode parentNode = TreeNode.createNode(rootDirectory);
-                System.out.println("\n" + parentNode);
+                TreeNode rootNode = TreeNode.createNode(rootDirectory);
+                System.out.println("\n" + rootNode);
             } else {
-                System.out.println("\nInvalid input\n");
+                System.out.println(INVALID_INPUT_ERROR);
             }
         }
         userInput.close();
